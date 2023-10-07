5caa8d8092d50faee71848b5615dc0d18102f19e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-07 20:50:12 +0300

    1. Build HomeScreen with its AppBar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        title: const Text.rich(
+          TextSpan(
+            text: 'News',
+            style: TextStyle(color: Colors.black),
+            children: [
+              TextSpan(
+                text: ' Cloud',
+                style: TextStyle(color: Colors.orange),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
