d1bb148ae61a7b47633eb9145ba9d4c7d4f6fa84
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-14 10:19:49 +0300

    3. Build Article Model

---
+++ lib/models/article.dart
@@ -0,0 +1,11 @@
+class Article {
+  final String title;
+  final String subtitle;
+  final String image;
+
+  const Article({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+}
