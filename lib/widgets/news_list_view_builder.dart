5b32fdfee6053ed5977d25a4e7eee1690355f320
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-14 18:29:54 +0300

    1. Use FutureBuilder & Convert its Widget back to Stateless Widget

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,52 +1,27 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-import '../models/article.dart';
 import '../services/news_service.dart';
 import 'news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
+class NewsListViewBuilder extends StatelessWidget {
   const NewsListViewBuilder({super.key});
 
   @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  var isLoading = true;
-  late final List<Article> articles;
-
-  @override
-  void initState() {
-    super.initState();
-    getNews();
-  }
-
-  Future<void> getNews() async {
-    articles = await NewsService(dio: Dio()).getNews();
-    setState(() => isLoading = false);
-  }
-
-  @override
   Widget build(context) {
-    return isLoading
-        ? const SliverFillRemaining(
-            hasScrollBody: false,
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          )
-        : articles.isEmpty
+    return FutureBuilder(
+      future: NewsService(dio: Dio()).getNews(),
+      builder: (context, snapshot) {
+        return snapshot.connectionState == ConnectionState.waiting
             ? const SliverFillRemaining(
-                hasScrollBody: false,
-                child: Center(
-                  child: Text('There was an error, try again later'),
-                ),
-              )
-            : NewsListView(articles: articles);
-    // return SliverList.builder(
-    //   itemBuilder: (context, index) => const NewsTile(),
-    //   itemCount: 10,
-    // );
+                child: Center(child: CircularProgressIndicator()))
+            : snapshot.data!.isEmpty
+                ? const SliverFillRemaining(
+                    child: Center(
+                        child: Text('There was an error, try again later')),
+                  )
+                : NewsListView(articles: snapshot.data!);
+      },
+    );
   }
 }
